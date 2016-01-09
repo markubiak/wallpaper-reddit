861f10a002b76602bd1522c9e5031bda45bdb376
Mark Kubiak <markubiak@gmail.com>
2016-01-08 23:28:55 -0500

    Try 2 for installing on Windows

--- setup.py
+++ setup.py
@@ -1,14 +1,17 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
         name='Wallpaper Reddit',
         version='2.0',
-        packages=['wpreddit'],
+        packages=find_packages(),
         url='https://www.github.com/markubiak/wallpaper-reddit',
         author='Mark Kubiak',
         author_email='mkubiak.dev@gmail.com',
         description='A utility that downloads wallpapers from reddit',
         install_requires=['Pillow>=3.0'],
+        package_data={
+            'wpreddit': ['fonts/Cantarell-Regular.otf']
+        },
         entry_points={
             'console_scripts': [
                 'wallpaper-reddit = wpreddit.main:run'
