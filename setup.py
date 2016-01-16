95e63ad6984868cc0328b74a71b5ddcc6485518a
Mark Kubiak <markubiak@gmail.com>
2016-01-16 01:32:48 -0500

    Updated version to match releases page

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
         name='wallpaper-reddit',
-        version='3.0',
+        version='3.0.0',
         packages=find_packages(),
         url='https://www.github.com/markubiak/wallpaper-reddit',
         author='Mark Kubiak',
