c0f9589b91fd9ab46baf551c89a990e03dbdfe4c
Mark Kubiak <markubiak@gmail.com>
2019-05-28 16:44:03 -0500

    Cleanup of connection.py

--- wpreddit/connection.py
+++ wpreddit/connection.py
@@ -1,61 +1,48 @@
 import ctypes
 import json
 import platform
+import requests
 import time
-import urllib.request
-from socket import timeout
-from urllib.error import HTTPError, URLError
 
 from wpreddit.common import log
 
 
-# in - string - web page url
-# out - boolean - connection status
-# checks whether the program can connect to the specified url
-def connected(url):
+# Checks whether the program can connect to the specified url
+# Out: (boolean) connection status for reddit.com
+def connected_to_reddit():
     try:
-        uaurl = urllib.request.Request(url,
-                                       headers={'User-Agent': 'wallpaper-reddit python script by /u/MarcusTheGreat7'})
-        url = urllib.request.urlopen(uaurl, timeout=3)
-        url.close()
+        r = requests.get("http://www.reddit.com/.json?limit=1",
+                         headers={'User-Agent': 'wallpaper-reddit python script: ' +
+                                                'github.com/markubiak/wallpaper-reddit'},
+                         timeout=(3, 10))
+        # Connected to something
+        if r.status_code != 200:
+            return False
+        # JSON format, check for 'kind' = "Listing" key
+        r_json = json.loads(r.text)
+        if r_json['kind'] != "Listing":
+            return False
         return True
-    except (HTTPError, URLError, timeout):
+    except (requests.ConnectionError, requests.ConnectTimeout, AttributeError, ValueError):
+        # JSON parsing failed, likely a redirect
         return False
 
 
-# out - boolean - connection status
-# checks whether the program can connect to reddit and is not being redirected
-def check_not_redirected():
-    try:
-        # Not reloading /etc/resolv.conf, since it will have to be reloaded for the function right before this is called
-        uaurl = urllib.request.Request('http://www.reddit.com/.json',
-                                       headers={'User-Agent': 'wallpaper-reddit python script by /u/MarcusTheGreat7'})
-        url = urllib.request.urlopen(uaurl, timeout=3)
-        json.loads(url.read().decode('utf8'))
-        url.close()
-        return True
-    except (HTTPError, URLError, timeout, AttributeError, ValueError):
-        return False
-
-
-# in - string, int, int - url to check for connection, how many attempts and at what interval to retry until connected
-# out - boolean - whether the connection was successfully establised
-# waits for a connection to the specified url, or returns False if no connection could be made in the time frame
+# Waits for a connection to the specified url, or returns False if no connection could be made in the time frame
+# In:  (int) number of attempts to connect int
+#      (int) interval to retry connection
+# Out: (boolean) whether the connection was successfully establised
 def wait_for_connection(tries, interval):
     log('Waiting for a connection...')
     for i in range(tries):
         if platform.system() == "Linux":
             # Reloads /etc/resolv.conf
-            # credit: http://stackoverflow.com/questions/21356781/urrlib2-urlopen-name-or-service-not-known-persists-when-starting-script-witho
+            # credit: http://stackoverflow.com/questions/21356781
             libc = ctypes.cdll.LoadLibrary('libc.so.6')
             res_init = libc.__res_init
             res_init()
         log('Attempt # ' + str(i + 1) + ' to connect...')
-        if connected("http://www.reddit.com"):
-            log('Connected to the internet, checking if you\'re being redirectied...')
-            if check_not_redirected():
-                log('No redirection.  Starting the main script...')
-                return True
-            log('Redirected.  Trying again...')
+        if connected_to_reddit():
+            return True
         time.sleep(interval)
     return False
